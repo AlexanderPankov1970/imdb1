6540fcffc16e64a331275e5878acdaca697b6465
AlexanderPankov1970 <pankovsasha7@gmail.com>
2023-03-14 13:21:37 +0300

    create page About

--- src/app/Providers.jsx
+++ src/app/Providers.jsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from "next-themes";
 export default function Providers({ children }) {
   return (
     <ThemeProvider enableSystem={true} attribute="class">
-      <div className="dark:bg-gray-600 dark:text-gray-300 text-gray-600 transition-colors duration-500 select-none min-h-screen">
+      <div className="dark:bg-gray-400 dark:text-gray-200 text-gray-600 transition-colors duration-500 select-none min-h-screen">
         {children}
       </div>
     </ThemeProvider>
