511eaca998131677ab26a97ae5b606457292b997
AlexanderPankov1970 <pankovsasha7@gmail.com>
2023-03-15 19:28:15 +0300

    create Card component

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Card from "./Card";
 
 export default function Results({ results }) {
   return (
-    <div className="text-green-700">
+    <div className="text-green-700 sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-ww-6xl mx-auto py-4">
       {results.map?.((result) => (
-        <div key={result.id}>{result.original_title}</div>
+        //<div key={result.id}>{result.original_title}</div>
+        <Card key={result.id} result={result} />
       ))}
     </div>
   );
